8bf092c9be76b060dbafacc6fe582c0790fea494
ronglexie <252254002@qq.com>
2018-04-06 23:07:06 +0800

    :sparkles:创建高复用服务响应对象

---
+++ src/main/java/com/ronglexie/mmall/common/ResponseEnum.java
@@ -0,0 +1,31 @@
+package com.ronglexie.mmall.common;
+
+/**
+ * 高复用服务响应对象Code枚举
+ *
+ * @author wxt.xqr
+ * @version 2018/4/6
+ */
+public enum ResponseEnum {
+
+	SUCCESS(0,"SUCCESS"),
+	ERROR(1,"ERROR"),
+	NEED_LOGIN(10,"NEED_LOGIN"),
+	ILLEGAL_ARGUMENT(2,"ILLEGAL_ARGUMENT");
+
+	private final Integer code;
+	private final String desc;
+
+	ResponseEnum(Integer code, String desc){
+		this.code = code;
+		this.desc = desc;
+	}
+
+	public Integer getCode() {
+		return code;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+}
---
+++ src/main/java/com/ronglexie/mmall/common/ServerResponse.java
@@ -0,0 +1,94 @@
+package com.ronglexie.mmall.common;
+
+import org.codehaus.jackson.annotate.JsonIgnore;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
+import java.io.Serializable;
+
+/**
+ * 高复用服务响应对象
+ *
+ * @author ronglexie
+ * @version 2018/4/6
+ */
+@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)//value值为NULL时忽略
+public class ServerResponse<T> implements Serializable {
+
+	private int status;
+	private String msg;
+	private T data;
+
+	private ServerResponse(int status) {
+		this.status = status;
+	}
+
+	private ServerResponse(int status, T data) {
+		this.status = status;
+		this.data = data;
+	}
+
+	private ServerResponse(int status, String msg) {
+		this.status = status;
+		this.msg = msg;
+	}
+
+	private ServerResponse(int status, String msg, T data) {
+		this.status = status;
+		this.msg = msg;
+		this.data = data;
+	}
+
+	/**
+	 * 是否成功
+	 *
+	 * @param  
+	 * @return boolean
+	 * @author wxt.xqr
+	 * @version 2018/4/6
+	 */
+	@JsonIgnore//不进行Json序列化
+	public boolean isSuccess(){
+		return this.status == ResponseEnum.SUCCESS.getCode();
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public static <T> ServerResponse<T> createBySuccess(){
+		return new ServerResponse<>(ResponseEnum.SUCCESS.getCode());
+	}
+
+	public static <T> ServerResponse<T> createBySuccessMsg(String msg){
+		return new ServerResponse<>(ResponseEnum.SUCCESS.getCode(), msg);
+	}
+
+	public static <T> ServerResponse<T> createBySuccess(T data){
+		return new ServerResponse<>(ResponseEnum.SUCCESS.getCode(), data);
+	}
+
+	public static <T> ServerResponse<T> createBySuccess(String msg, T data){
+		return new ServerResponse<>(ResponseEnum.SUCCESS.getCode(), msg, data);
+	}
+
+	public static <T> ServerResponse<T> createByError(){
+		return new ServerResponse<>(ResponseEnum.ERROR.getCode(), ResponseEnum.ERROR.getDesc());
+	}
+
+	public static <T> ServerResponse<T> createByErrorMsg(String errMsg){
+		return new ServerResponse<>(ResponseEnum.ERROR.getCode(), errMsg);
+	}
+
+	public static <T> ServerResponse<T> createByErrorCodeMsg(int errCode, String errMsg){
+		return new ServerResponse<>(errCode, errMsg);
+	}
+
+}
